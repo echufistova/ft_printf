079cf29652ef367ddd447c6e89e8999805afb532
Yevheniya CHUFISTOVA <ychufist@e1r10p3.unit.ua>
2018-09-12 11:38:28 +0300

    38720

--- ft_print_p.c
+++ ft_print_p.c
@@ -14,9 +14,22 @@ char *ft_print_p(va_list ap, t_flagsntype flntp, char *res)
 	if (n != 0)
 	    s = ft_itoa_base_uns(n, 16, flntp);
 	else
+	{
 		s = "0x0";
-	flntp.number -= 2;
-	res = (flag_space(res, flntp, n));
-    res = ft_strjoin(insertoct(res, flntp, n), s);
+		flntp.number--;
+	}
+	flntp.number--;
+	if (flntp.minus == 1 || flntp.noll == 1)
+	{
+		if (flntp.noll == 1)
+			flntp.number--;
+		//res = ft_strjoin(insertoct(res, flntp, n), s);
+		res = (flag_space(ft_strjoin(insertoct(res, flntp, n), s), flntp, n));
+	}
+	else
+	{
+		//res = (flag_space(res, flntp, n));
+		res = ft_strjoin(insertoct(flag_space(res, flntp, n), flntp, n), s);
+	}
     return (res);
 }
