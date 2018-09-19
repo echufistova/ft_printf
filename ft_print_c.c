23f2c35c5d854b45ec525065ece5ebd20875740f
Yevheniya CHUFISTOVA <ychufist@e1r8p4.unit.ua>
2018-09-19 16:00:51 +0300

    33019

--- ft_print_c.c
+++ ft_print_c.c
@@ -7,14 +7,12 @@
 
 void ft_printnull(char *res, t_flagsntype flntp, unsigned int c, wchar_t w)
 {
-    //if (c == '\0')
+    if (flntp.minus == 0)
     ft_putstr(res);
-    if (flntp.minus == 0) {
         if (flntp.type == 'c')
             ft_putchar(c);
         else if (flntp.type == 'C' || flntp.hljz.l == 1)
             ft_putchar_w(w);
-    }
 }
 
 char *ft_print_c(va_list ap, t_flagsntype flntp, char *res, int *len)
@@ -39,16 +37,28 @@ char *ft_print_c(va_list ap, t_flagsntype flntp, char *res, int *len)
             p = ' ';
         if (flntp.minus == 1) {
             i = 1;
-            res = ft_add_char(res, (char) c);
+            if (flntp.type == 'c')
+                res = ft_add_char(res, (char) c);
+            else
+                {
+                ft_putstr(res);
+                    (*len)++;
+                ft_bzero(res, ft_strlen(res));
+                ft_printnull(res, flntp, c, w);
+            }
             while (i++ < flntp.number)
                 res = ft_add_char(res, ' ');
-        } else {
+           // if (flntp.type == 'c')
+                ft_putstr(res);
+        }
+        else {
             while (i++ < flntp.number - 1)
                 res = ft_add_char(res, p);
         }
-        //i = 0;
+        //return (res);
     }
-    ft_printnull(res, flntp, c, w);
+    if (!flntp.minus)
+        ft_printnull(res, flntp, c, w);
     (flntp.minus == 0) ? (*len)++ : (*len); //+ ((c == '\0') ? 1 : 0);
     return (res);
 }
