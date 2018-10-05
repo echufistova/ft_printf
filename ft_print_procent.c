6f75ed9e79458ae8a48654d71c15bdadebf473fa
ychufist <ychufist@student.unit.ua>
2018-10-05 18:08:04 +0300

    print_procent-norme

--- ft_print_procent.c
+++ ft_print_procent.c
@@ -1,25 +1,32 @@
-//
-// Created by echufy on 08.08.18.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_procent.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ychufist <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/05 18:06:30 by ychufist          #+#    #+#             */
+/*   Updated: 2018/10/05 18:07:19 by ychufist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_printf.h"
 
-char *procent(va_list ap, const char *format, unsigned int *i, char *res)
+char	*procent(va_list ap, const char *format, unsigned int *i, char *res)
 {
-    char *res1;
+	char *res1;
 
-    while (format[*i] != '%' && format[*i] != '\0')
-    {
-        ft_putchar(format[*i]);
-        (*i)++;
-    }
-    //va_arg(ap, int);
-    (*i)++;
-    if (format[*i] == '%' && format[*i] != '%')
-    {
-        res1 = procent(ap, format, i, res);
-        res = ft_strjoin_free(&res, &res1);
-    }
-    return (res);
+	while (format[*i] != '%' && format[*i] != '\0')
+	{
+		ft_putchar(format[*i]);
+		(*i)++;
+	}
+	(*i)++;
+	if (format[*i] == '%' && format[*i] != '%')
+	{
+		res1 = procent(ap, format, i, res);
+		res = ft_strjoin_free(&res, &res1);
+	}
+	return (res);
 }
